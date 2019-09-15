d3b1433ddafd6c9ec8a38dc3b0c5475b0051cc31
Xavi Galarza <xagaler@gmail.com>
2019-09-15 18:14:40 +0100

    P03 and P04 done. Comments added all along.

--- src/main/scala/problems/lists.scala
+++ src/main/scala/problems/lists.scala
@@ -1,10 +1,21 @@
 package problems
 
+import annotation.tailrec
+
 object lists {
 
-  //P01: Find the last element of a list.
+  //// P01: Find the last element of a list.
+  //
+  //  Find the last element of a list.
+  //     Example:
+  //     scala> last(List(1, 1, 2, 3, 5, 8))
+  //     res0: Int = 8
+  //
+  //// Built-in solution
   val last_element_builtin = (input_list: List[Any]) => input_list.last
 
+  //// Recursive approach
+  @tailrec
   def last_element_func(input_list: List[Any], result: Any = null): Any = {
     input_list match {
       case Nil => result
@@ -12,22 +23,92 @@ object lists {
     }
   }
 
-  //P02: Find the last but one element of a list.
-  val penultime_builtin = (input_list: List[Any]) => input_list.init.last
+  //// P02: Find the last but one element of a list.
+  //
+  //     Example:
+  //     scala> penultimate(List(1, 1, 2, 3, 5, 8))
+  //     res0: Int = 5
+  //
+  //// Built-in solution
+  val penultimate_builtin = (input_list: List[Any]) => input_list.init.last
 
-  def penultime[A](input_list:List[A]): A = {
+  //// Recursive approach
+  @tailrec
+  def penultimate[A](input_list:List[A]): A = {
     input_list match {
       case head :: _ :: Nil => head
-      case head :: tail => penultime(tail)
+      case head :: tail => penultimate(tail)
       case _ => throw new NoSuchElementException
     }
   }
 
-  //TO-DO
-  //P03: Find the Kth element of a list.
+  //// P03: Find the Kth element of a list.
+  //
+  //     By convention, the first element in the list is element 0.
+  //
+  //     Example:
+  //     scala> nth(2, List(1, 1, 2, 3, 5, 8))
+  //     res0: Int = 2
+  //
+  //// Built-in approach
+  val nth_builtin = (element: Int, input_list: List[Any]) => {
+    if(element >= 0 && element <= input_list.size) {input_list(element-1)}
+    else {throw new NoSuchElementException}
+  }
 
-  //TO-DO
-  //P04: Find the number of elements of a list.
+  //// Recursive approach
+  @tailrec
+  def nth_recursive[A](element: Int, input_list:List[A]): A = {
+    (element-1,input_list) match {
+      case (0, head :: _ )    => head
+      case (_, head :: tail ) => nth_recursive(element-1, tail)
+      case (_, Nil) => throw new NoSuchElementException
+    }
+  }
+
+  //// Recursive approach with counter
+  def nth_counter[A](element: Int, input_list:List[A]): A = {
+    @tailrec def _nth_counter[A](element: Int, input_list:List[A], count: Int): A = {
+      if (count > input_list.size) throw new NoSuchElementException
+      else {
+        count match {
+          case 0 => input_list.head
+          case _ => _nth_counter(element-1, input_list.tail, count-1)
+        }
+      }
+    }
+    _nth_counter(element, input_list, element-1)
+  }
+
+  //// P04: Find the number of elements of a list.
+  //
+  //     Example:
+  //     scala> length(List(1, 1, 2, 3, 5, 8))
+  //     res0: Int = 6
+  //
+  //// Built-in solution
+  val length_builtin = (input_list: List[Any]) => {
+    if(input_list == Nil) 0
+    else input_list.size
+  }
+
+  //// Recursive approach
+  def length_recursive[A](input_list: List[A]): Int = {
+    @tailrec def _length_recursive[A](input_list: List[A], count: Int = 0): Int = {
+      input_list match {
+        case Nil => count
+        case head :: tail => _length_recursive(tail, count+1)
+      }
+    }
+    _length_recursive(input_list)
+  }
+
+  //// Pure functional solution
+  val length_functional = (input_list: List[Any]) => {
+    input_list.foldLeft(0){ (c, _) => c + 1 }
+  }
+
+
 
   //TO-DO
   //P05: Reverse a list.
--- src/test/scala/problems/listsTest.scala
+++ src/test/scala/problems/listsTest.scala
@@ -7,7 +7,7 @@ import problems.lists._
 class listsTest extends FunSpec with Matchers {
 
   describe("The P01"){
-    describe("with the simplest method"){
+    describe("with the built-in method"){
       it("Should return the last element of a list :: Integer"){
       val input = List(1,2,6,99)
       val result = lists .last_element_builtin(input)
@@ -43,14 +43,14 @@ class listsTest extends FunSpec with Matchers {
     describe("with the built-in solution"){
       it("should return the penultime element of a list :: Integer"){
         val input = List(0,1,3,5,7)
-        val result = lists .penultime_builtin(input)
+        val result = lists .penultimate_builtin(input)
 
         result should be (5)
 
       }
       it("should return the penultime element of a list :: String"){
         val input = List("one","three","last")
-        val result = penultime(input)
+        val result = penultimate(input)
 
         result should be ("three")
 
@@ -59,22 +59,141 @@ class listsTest extends FunSpec with Matchers {
     describe("with the recursive approach"){
       it("should return the penultime element of a list :: Integer"){
         val input = List(0,5,3,19,8)
-        val result = penultime(input)
+        val result = penultimate(input)
 
         result should be (19)
 
       }
       it("should return the penultime element of a list :: String"){
         val input = List("one","penultime","last")
-        val result = penultime(input)
+        val result = penultimate(input)
 
         result should be ("penultime")
       }
       it("should return a NoSuchElementException with a null list"){
         val input = List()
         an [NoSuchElementException] should be thrownBy {
-          penultime(input)
+          penultimate(input)
         }
+      }
+    }
+  }
+  describe("The P03"){
+    describe("using a counter"){
+      it("should return the nth element of a list :: Integer"){
+        val input = List(1,2,5,8,9,0)
+        val result = nth_counter(4, input)
+
+        result should be (8)
+
+      }
+      it("should return the nth element of a list :: String"){
+        val input = List("hi","bye","this")
+        val result = nth_counter(3, input)
+
+        result should be ("this")
+
+      }
+      it("should return NoSuchElementException if the nth element is not in the list"){
+        val input = List(1,4,5)
+
+        an [NoSuchElementException] should be thrownBy {
+          nth_counter(5,input)
+        }
+      }
+    }
+    describe("using the simplest recursive method"){
+      it("should return the nth element of a list :: Integer"){
+        val input = List(1,2,5,8,9,0)
+        val result = nth_recursive(4, input)
+
+        result should be (8)
+
+      }
+      it("should return the nth element of a list :: String"){
+        val input = List("hi","bye","this")
+        val result = nth_recursive(3, input)
+
+        result should be ("this")
+
+      }
+      it("should return NoSuchElementException if the nth element is not in the list"){
+        val input = List(1,4,5)
+
+        an [NoSuchElementException] should be thrownBy {
+          nth_recursive(5,input)
+        }
+      }
+    }
+    describe("using the built-in method"){
+      it("should return the nth element of a list :: Integer"){
+        val input = List(1,2,5,8,9,0)
+        val result = nth_builtin(4, input)
+
+        result should be (8)
+
+      }
+      it("should return the nth element of a list :: String"){
+        val input = List("hi","bye","this")
+        val result = nth_builtin(3, input)
+
+        result should be ("this")
+
+      }
+      it("should return NoSuchElementException if the nth element is not in the list"){
+        val input = List(1,4,5)
+
+        an [NoSuchElementException] should be thrownBy {
+          nth_builtin(5,input)
+        }
+      }
+    }
+  }
+  describe("The P04"){
+    describe("using the built-in method"){
+      it("should return the size of the input list"){
+        val input = List(0,2,3,4,4,5)
+        val result = length_builtin(input)
+
+        result should be (6)
+      }
+      it("should return 0 when the list is empty"){
+        val input = List()
+        val result = length_builtin(input)
+
+        result should be (0)
+
+      }
+    }
+    describe("using recursive approach"){
+      it("should return the size of the input list :: List has values"){
+        val input = List(1,2,3,4,5)
+        val result = length_recursive(input)
+
+        result should be (5)
+
+      }
+      it("should return the size of the input list == 0 :: List is empty"){
+        val input = List()
+        val result = length_recursive(input)
+
+        result should be (0)
+
+      }
+    }
+    describe("using the more pure functional approach"){
+      it("should return the size of the input list :: List has values"){
+        val input = List(1,2,3,4,5)
+        val result = length_functional(input)
+
+        result should be (5)
+      }
+      it("should return the size of the input list == 0 :: List is empty"){
+        val input = List()
+        val result = length_functional(input)
+
+        result should be (0)
+
       }
     }
   }
